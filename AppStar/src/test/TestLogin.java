1ee133158294ad21b2823f017dc121510f1d5fcc
deamon96 <36306399+deamon96@users.noreply.github.com>
2018-05-23 20:25:55 +0200

    AppStar Test
    Prova di test per il Login.
    Proveremo a fare i test in maniera più efficace studiando dalle slide e dal codice del prof. De Angelis di ISPW.

---
+++ AppStar/src/test/TestLogin.java
@@ -0,0 +1,51 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static persistence.UtenteDao.controlloAccount;
+
+public class TestLogin {
+    private ArrayList<String> infoLorenzo = new ArrayList<>(6);
+    private ArrayList<String> infoGiacomo = new ArrayList<>(6);
+
+    private void definisciInfoLorenzo(){
+        infoLorenzo.add("Lorenzo"); infoLorenzo.add("Zara"); infoLorenzo.add("lorzar"); infoLorenzo.add("lorzar");
+        infoLorenzo.add("lorenzo.zara96@gmail.com"); infoLorenzo.add("amministratore");
+        }
+    private void definisciInfoGiacomo(){
+        infoGiacomo.add("Giacomo"); infoGiacomo.add("Rossi"); infoGiacomo.add("red_jack"); infoGiacomo.add("redjack");
+        infoGiacomo.add("giacomo.redjack@gmail.com"); infoGiacomo.add("notAmministratore");
+    }
+
+    @Test
+    public void adminVerificato() throws SQLException{
+        System.out.println("----------VERIFICA DI LOGIN DA PARTE DI UN AMMINISTRATORE REGISTRATO----------");
+        ArrayList<String> infoUtente;
+        infoUtente = controlloAccount("lorzar", "lorzar");
+        definisciInfoLorenzo();
+        Assert.assertEquals(infoLorenzo, infoUtente);
+    }
+
+    @Test
+    public void adminNonVerificato() throws SQLException{
+        System.out.println("----------VERIFICA DI LOGIN DA PARTE DI UN AMMINISTRATORE NON REGISTRATO----------");
+        ArrayList<String> infoUtente;
+        infoUtente = controlloAccount("noName", "noPass");
+        definisciInfoLorenzo();
+        Assert.assertNotEquals(infoLorenzo, infoUtente);
+    }
+
+    @Test
+    public void utenteVerificato() throws SQLException{
+        System.out.println("----------VERIFICA DI LOGIN DA PARTE DI UN UTENTE REGISTRATO----------");
+        ArrayList<String> infoUtente;
+        infoUtente = controlloAccount("red_jack", "redjack");
+        definisciInfoGiacomo();
+        Assert.assertEquals(infoGiacomo, infoUtente);
+    }
+}
