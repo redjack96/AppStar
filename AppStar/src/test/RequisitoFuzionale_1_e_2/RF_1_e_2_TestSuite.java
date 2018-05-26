8bc224739525656d7087e26a6423856df09342cd
deamon96 <36306399+deamon96@users.noreply.github.com>
2018-05-26 16:32:16 +0200

    AppStar Test
    Aggiunto allRF_TestSuite. Fai partire questo per avviare tutti i test.

--- AppStar/src/test/RequisitoFuzionale_1_e_2/RF_1_e_2_TestSuite.java
+++ AppStar/src/test/RequisitoFuzionale_1_e_2/RF_1_e_2_TestSuite.java
@@ -3,7 +3,6 @@ package test.RequisitoFuzionale_1_e_2;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-import test.RequisitoFuzionale_1_e_2.BeforeAfterLoginTest;
 
 @RunWith(value=Suite.class)
 //@SuiteClasses(value={testClass.class, ...})
